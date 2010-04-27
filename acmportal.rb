24ce3bcf761b64036ffb20cde62c9f6784f89913
Sho Hashimoto <hashimoto@shokai.org>
2010-04-27 19:00:47 +0900

    download from acmportal

---
+++ acmportal.rb
@@ -0,0 +1,34 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'open-uri'
+require 'kconv'
+require 'hpricot'
+require 'pp'
+
+if ARGV.size < 1
+  puts 'download pdf files'
+  puts 'ruby acmportal.rb "http://portal.acm.org/toc.cfm?id=1517664&coll=ACM&dl=ACM&type=proceeding&idx=SERIES11433&part=series&WantType=Proceedings&title=TEI&CFID=86099218&CFTOKEN=41539595"'
+  exit 1
+end
+uri = ARGV.shift
+
+doc = Hpricot(open(uri).read.toutf8)
+
+links = doc/:a
+pdfs = Array.new
+for i in 0...links.length
+  pdfs << "http://portal.acm.org/"+links[i][:href].to_s
+end
+
+pdfs = pdfs.uniq.delete_if{|url|
+  !(url =~ /pdf/)
+}
+
+pdfs.each{|url|
+  puts url
+  #puts `wget "#{url}"`
+}
+
+Dir.glob("*\?*").each{|name|
+  `mv "#{name}" #{name.split(/\?/).first}`
+}
